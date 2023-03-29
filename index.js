e8c573bca9b5118149a446aeca0314d48a2b8630
alextedtech <alextedtech@gmail.com>
2023-03-29 12:33:35 -0700

    Fixed Password Generator funcitons

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 
 let password1 = document.getElementById("password-1")
 let password2 = document.getElementById("password-2")
-
+let gather = ""
 
 
 function pwGen(){
+    gather =""
     for ( let i = 0; i < 15; i++){
-        let passwordGen = Math.floor(Math.random() * characters.length)
-         password1.textContent += characters[passwordGen]
+        let passwordGen = Math.floor(Math.random() * characters.length); 
+        gather +=characters[passwordGen];
+        
     }
+    return gather
+    
+    
 }
 
+function pwRequest1(){
+    
+    password1.textContent = pwGen();
+}
+
+function pwRequest2(){
+    password2.textContent = pwGen();
+}
